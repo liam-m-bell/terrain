e4aa8f3d3d2cf8298cff58d96cb7dcf53e48b870
Liam Bell <liam2159@gmail.com>
2024-01-14 23:27:36 +0000

    Diamond Square working

--- mesh.c
+++ mesh.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+float MAX_HEIGHT = 600.0f;
 
 // Structure to represent a 3D vector
 typedef struct {
@@ -34,7 +37,7 @@ float **generateRandomHeightfield(float **heightfield, const int size){
 }
 
 // Create a mesh from a heightfield
-Vector *createMeshFromHeightfield(float **heightfield, const int size) {
+Vector *createMeshFromHeightfield(float **heightfield, const int size){
     Vector *vertices = (Vector*)malloc(size * size * sizeof(Vector));
 
     for (int x = 0; x < size; x++) {
@@ -50,7 +53,7 @@ Vector *createMeshFromHeightfield(float **heightfield, const int size) {
 }
 
 // Export mesh as OBJ file
-void exportMeshAsObj(Vector *vertices, const char *filename, const int size) {
+void exportMeshAsObj(Vector *vertices, const char *filename, const int size){
     FILE* objFile = fopen(filename, "w");
     if (!objFile) {
         fprintf(stderr, "Error: Unable to open file for writing: %s\n", filename);
@@ -74,14 +77,92 @@ void exportMeshAsObj(Vector *vertices, const char *filename, const int size) {
     fclose(objFile);
 }
 
-int main() {
-    const int size = 100;
+// Diamond Square subdivision algorithm
+float randomHeight()
+{
+    return (((float)rand() / RAND_MAX) * 2.0f * MAX_HEIGHT) -  MAX_HEIGHT;
+}
+
+void squareStep(float **heightfield, const int size, int x, int z, int offset, float scale){
+    float total = 0.0f;
+    int count = 0;
+
+    if (x - offset >= 0) {
+        total += heightfield[x - offset][z];
+        count++;
+    }
+    if (x + offset < size) {
+        total += heightfield[x + offset][z];
+        count++;
+    }
+    if (z - offset >= 0) {
+        total += heightfield[x][z - offset];
+        count++;
+    }
+    if (z + offset < size) {
+        total += heightfield[x][z + offset];
+        count++;
+    }
+
+    float average = total / (float)count;
+    heightfield[x][z] = average + randomHeight() * scale;
+}
+
+void diamondStep(float **heightfield, const int size, int x, int z, int offset, float scale){
+    float average = (heightfield[x-offset][z-offset] +
+        heightfield[x+offset][z-offset] + 
+        heightfield[x-offset][z+offset] +
+        heightfield[x+offset][z+offset]) / 4.0f;
+    
+    heightfield[x][z] = average + randomHeight() * scale;
+}
+
+void diamondSquareStep(float **heightfield, const int size, int width, float scale){
+    int half = width / 2;
+
+    for (int x = half; x < size; x += width){
+        for (int z = half; z < size; z += width){
+            diamondStep(heightfield, size, x, z, half, scale);
+        }
+    }
+
+    for (int x = half; x < size; x += width){
+        for (int z = half; z < size; z += width){
+            squareStep(heightfield, size, x, z - half, half, scale);
+            squareStep(heightfield, size, x, z + half, half, scale);
+            squareStep(heightfield, size, x - half, z, half, scale);
+            squareStep(heightfield, size, x + half, z, half, scale);
+        }
+    }
+}
+
+void diamondSquare(float **heightfield, const int size, const float roughness){
+    // Initialise corners
+    heightfield[0][0] = randomHeight();
+    heightfield[0][size - 1] = randomHeight();
+    heightfield[size - 1][0] = randomHeight();
+    heightfield[size - 1][size - 1] = randomHeight();
+
+    int width = size - 1;
+    float scale = 1.0f;
+
+    while (width > 1){
+        diamondSquareStep(heightfield, size, width, scale);
+        width /= 2;
+        scale *= roughness; 
+    }
+}
+
+int main(){
+    const int n = 10;
+    const int size = pow(2, n) + 1;
 
     // Create heightfield
     float **heightfield = createHeightfield(size);
 
     // Generate heightfield
-    generateRandomHeightfield(heightfield, size);
+    //generateRandomHeightfield(heightfield, size);
+    diamondSquare(heightfield, size, 0.4);
 
     // Create mesh from heightfield
     Vector *mesh = createMeshFromHeightfield(heightfield, size);
