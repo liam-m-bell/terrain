2268d2c20a1cded43dd2ecc0272ffff0d512b354
Liam Bell <liam2159@gmail.com>
2024-01-15 19:20:43 +0000

    Added mesh headers

--- mesh.c
+++ mesh.c
@@ -1,44 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
 
-#include "diamond_square.h"
-
-// Structure to represent a 3D vector
-typedef struct{
-    float x, y, z;
-} Vector;
-
-typedef struct{
-    int v0, v1, v2;
-} Triangle;
-
-typedef struct{
-    int vertexCount;
-    Vector *vertices;
-
-    int faceCount;
-    Triangle *faces;
-} Mesh;
-
-// Allocates memory for a heightfield
-float **createHeightfield(const int size){
-    float **a = (float**)malloc(size * sizeof(float*));
-    for (int i = 0; i < size; i++){
-        a[i] = (float*)malloc(size * sizeof(float));
-    }
-
-    return a;
-}
-
-// Deallocates memory for a heightfield
-void freeHeightfield(float **a, const int size){
-    for (int i = 0; i < size; i++) {
-        free(a[i]);
-    }
-    free(a);
-}
+#include "mesh.h"
 
 // Create a mesh from a heightfield
 Mesh *createMeshFromHeightfield(float **heightfield, const int size){
@@ -221,29 +184,4 @@ void exportMeshAsObj(Mesh *mesh, const char *filename){
     }
 
     fclose(objFile);
-}
-
-int main(){
-    srand(time(NULL));
-
-    const int n = 7;
-    const int size = pow(2, n) + 1;
-
-    // Create heightfield
-    float **heightfield = createHeightfield(size);
-
-    // Generate heightfield
-    diamondSquare(heightfield, size, 0.45);
-
-    // Create mesh from heightfield
-    Mesh *mesh = createMeshFromHeightfield(heightfield, size);
-
-    // Export mesh as OBJ file
-    const char* objFilename = "output.obj";
-    exportMeshAsObj(mesh, objFilename);
-
-    free(mesh);
-    freeHeightfield(heightfield, size);
-
-    return 0;
 }
---
+++ vector.h
@@ -0,0 +1,9 @@
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+// Structure to represent a 3D vector
+typedef struct{
+    float x, y, z;
+} Vector;
+
+#endif
