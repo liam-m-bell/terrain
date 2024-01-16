54e0c1f32b658c4b8955301d71870f34760b8ef6
Liam Bell <liam2159@gmail.com>
2024-01-16 19:00:21 +0000

    Added vector functions

--- vector.h
+++ vector.h
@@ -1,9 +1,22 @@
 #ifndef VECTOR_H_
 #define VECTOR_H_
 
+#include <math.h>
+
 // Structure to represent a 3D vector
 typedef struct{
     float x, y, z;
 } Vector;
+
+Vector vector3(float x, float y, float z);
+Vector vector2(float x, float y);
+
+float lenthSquared(Vector v);
+float length(Vector v);
+Vector normalise(Vector v);
+float dot(Vector a, Vector b);
+Vector cross(Vector a, Vector b);
+Vector scale(Vector v, float scalar);
+Vector add(Vector a, Vector b);
 
 #endif
